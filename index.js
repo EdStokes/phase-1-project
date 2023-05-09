c8dee34e4b9d81d4e2e41c05f82c17cf29744528
Ed Stokes <edstokes96@gmail.com>
2023-05-08 22:24:41 -0500

    added site infor event to code

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("App is running");
+
+//Selects which page is displayed in the DOM
+function showPage(page) {
+    document.querySelectorAll("div").forEach(div => {
+       div.style.display = 'none'; 
+    })
+    document.querySelector(`#${page}`).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = function() {
+            showPage(this.dataset.page);
+        }
+    })
+})
+
+//site info
+
+function siteInfo() {
+    console.log("add site info")
+}
+
+document.addEventListener('DOMContenLoaded', function(){
+    document.querySelector('#siteInfo').onclick = siteInfo;
+})
